b3a4c405b10f8ad45bb4b7d590e592ba8b329485
Tyler Payne <169082073+edamama@users.noreply.github.com>
2024-07-18 11:54:35 -0500

    3
    Now renders shape correctly in svg file.  May need to tweak things.

--- libraries/generateSvg.js
+++ libraries/generateSvg.js
@@ -1,25 +1,58 @@
+
+
+
+
+
+
 function generateSvg(data){ //insert needed code
 
-    return `
-    
-    
-    
-    
-    
-    
-    
-    
+    if(data.shape === "Triangle"){
+
+        return `
+        
+        
+        <svg height="300" width="500" xmlns="http://www.w3.org/2000/svg">
+        
+            <polygon points="250,60 100,400 400,400" style="fill:${data.shapeColor}"/>
+
+            <text x="250" y="250" font-size="60" text-anchor="middle" fill="${data.textColor}">${data.characters}</text>
+
+        </svg>
     
     `
+    } else if(data.shape === "Circle"){
 
+        return`
 
+        <svg version="1.1" width="300" height="300" xmlns="http://www.w3.org/2000/svg">
 
+            <circle cx="150" cy="140" r="100" width="100%" fill="${data.shapeColor}"/>
+
+            <text x="150" y="160" font-size="60" text-anchor="middle" fill="${data.textColor}">${data.characters}</text>
+
+        </svg>
+
+        `
+
+    } else{ //square
+
+        return `
+
+        <svg version="1.1" width="300" height="300" xmlns="http://www.w3.org/2000/svg">
+
+            <rect width="300" height="300" x="0" y="0" rx="0" ry="0" fill="${data.shapeColor}"/>
+
+            <text x="150" y="160" font-size="60" text-anchor="middle" fill="${data.textColor}">${data.characters}</text>
+
+        </svg>
+
+        `
+
+    }
 
 }
 
 //add switch case scenarios probably
-
-
 
 
 
--- libraries/shapes.js
+++ libraries/shapes.js
@@ -50,3 +50,5 @@ function Triangle(){
     }
 
 }
+
+module.exports = {Circle,Square,Triangle};
